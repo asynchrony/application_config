da490633c0a5755a2059114731fb5e17a0366ad0
jason mayer <jason.mayer@gmail.com>
2015-11-24 11:05:32 -0600

    changing rails root to Rails 3 root

--- init.rb
+++ init.rb
@@ -2,11 +2,14 @@ require 'application_config'
 require 'application_config/data_structures/always_null_node'
 require 'application_config/data_structures/nested_hash'
 require 'application_config/data_structures/value_node'
+
 begin
   config = ApplicationConfig.new
-  [RAILS_ROOT +  "/config/application.yml", RAILS_ROOT + "/config/environments/application-#{RAILS_ENV}.yml"].each do |config_file|
-    next unless File.exists?(config_file)
-    config.add(ERB.new(IO.read(config_file)).result)
+
+  ["/config/application.yml", "/config/environments/application-#{Rails.env}.yml"].each do |config_file|
+    file_loc = File.join(Rails.root, config_file)
+    next unless File.exists?(file_loc)
+    config.add(ERB.new(IO.read(file_loc)).result)
   end
   ApplicationConfig.singleton(config)
 end
